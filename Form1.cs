47a06ec48766445bd7e32b46c86e0e315ca2256f
Laila Shihada <764700695@nzse.ac.nz>
2023-01-31 23:16:30 +1300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SWD604_ClassActivity_Singleton_31_01_2023.Modules;
+
+namespace SWD604_ClassActivity_Singleton_31_01_2023
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			CreateDatabaseConnection DataBaseConnect = CreateDatabaseConnection.GetDB_Instance();
+			MessageBox.Show(DataBaseConnect.connectionString());
+			
+		}
+	}
+}
---
+++ Modules/CreateDatabaseConnection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SWD604_ClassActivity_Singleton_31_01_2023.Modules
+{
+	sealed class CreateDatabaseConnection : DatabaseConnection
+	{
+		private CreateDatabaseConnection() { }
+		private static CreateDatabaseConnection DB_instance;
+        public static CreateDatabaseConnection GetDB_Instance()
+        {
+            if (DB_instance == null)
+            {
+                DB_instance = new CreateDatabaseConnection();
+            }
+            MessageBox.Show("Singleton DB instance is created!");
+            return DB_instance;
+        }
+
+        SqlConnection conn;
+        public bool Open()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(connectionString());
+                sqlConnection.Open();
+                conn = new SqlConnection(connectionString());
+                conn.Open();
+                return true;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Connection Error ! " + er.Message, "Information");
+            }
+            return false;
+        }
+        public void Close()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+        public string connectionString()
+        {
+            return getstrProvider();
+        }
+
+        public DataSet ExecuteDataSet(string sql)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                da.Fill(ds, "result");
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+        public int ExecuteReader(string sql)
+        {
+            try
+            {
+                SqlDataReader reader;
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                reader = cmd.ExecuteReader();
+                int counter = 0;
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                        counter++;
+                    return counter;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return -1;
+        }
+
+        public int ExecuteNonQuery(string sql)
+        {
+            try
+            {
+                int affected;
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //cmd.CommandText = sql;
+                affected = cmd.ExecuteNonQuery();
+                return affected;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return -1;
+        }
+
+    }
+}
